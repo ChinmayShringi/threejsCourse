f04ab9c38ed1b2f89276a146743ca937fd28c514
Chinmay Shringi <31031919+ChinmayShringi@users.noreply.github.com>
2021-05-31 15:11:30 +0530

    completed Text

--- Text3D/src/script.js
+++ Text3D/src/script.js
@@ -3,6 +3,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import gsap from "gsap";
 import * as dat from "dat.gui";
+import { Mesh } from "three";
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -11,12 +12,64 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
+ * Font
+ */
+
+const fontLoader = new THREE.FontLoader();
+
+fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
+  const textGeometry = new THREE.TextBufferGeometry("SUP!", {
+    font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+  //   To center
+  //   textGeometry.computeBoundingBox();
+  //   textGeometry.translate(
+  //     -textGeometry.boundingBox.max.x / 2,
+  //     -textGeometry.boundingBox.max.y / 2,
+  //     -textGeometry.boundingBox.max.z / 2
+  //   );
+  textGeometry.center();
+  const material = new THREE.MeshNormalMaterial();
+  const text = new THREE.Mesh(textGeometry, material);
+  scene.add(text);
+
+  const donutGrometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45);
+
+  for (let i = 0; i < 1000; i++) {
+    const donut = new THREE.Mesh(donutGrometry, material);
+
+    donut.position.set(
+      (Math.random() - 0.5) * 10,
+      (Math.random() - 0.5) * 10,
+      (Math.random() - 0.5) * 10
+    );
+
+    donut.rotation.set(
+      Math.random() * Math.PI,
+      Math.random() * Math.PI,
+      Math.random() * Math.PI
+    );
+    let scale = Math.random() / 2;
+    donut.scale.set(scale, scale, scale);
+    scene.add(donut);
+  }
+});
+
+/**
  * Object
  */
-const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
 
 /**
  * Sizes
