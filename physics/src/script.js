007c6ff9bd9ce90ec4945df8239995bb02b9b79a
Chinmay Shringi <31031919+ChinmayShringi@users.noreply.github.com>
2021-06-01 22:05:25 +0530

    add light physics

--- physics/src/script.js
+++ physics/src/script.js
@@ -27,12 +27,91 @@ const envMapTexture = cubeTextureLoader.load([
 ]);
 
 /**
+ * Physics
+ */
+// World
+const world = new CANNON.World();
+world.gravity.set(0, -9.82, 0);
+
+// Material
+const concreteMaterial = new CANNON.Material("concrete");
+const plasticMaterial = new CANNON.Material("plastic");
+
+const concPlasticContactMaterial = new CANNON.ContactMaterial(
+  concreteMaterial,
+  plasticMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7,
+  }
+);
+world.addContactMaterial(concPlasticContactMaterial);
+
+// Sphere
+const sphereShape = new CANNON.Sphere(0.5);
+const sphereBody = new CANNON.Body({
+  mass: 1,
+  position: new CANNON.Vec3(0, 3, 0),
+  shape: sphereShape,
+  material: plasticMaterial,
+});
+world.addBody(sphereBody);
+
+// floor
+const floorShape = new CANNON.Plane();
+const floorBody = new CANNON.Body();
+floorBody.mass = 0;
+floorBody.addShape(floorShape);
+floorBody.material = concreteMaterial;
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
+world.addBody(floorBody);
+
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+  new THREE.PlaneBufferGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: "#777777",
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: envMapTexture,
+  })
+);
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
+
+/**
  * Object
  */
-const geometry = new THREE.SphereBufferGeometry(1, 32, 32);
-const material = new THREE.MeshBasicMaterial();
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const geometry = new THREE.SphereBufferGeometry(0.5, 32, 32);
+const material = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: envMapTexture,
+});
+const sphere = new THREE.Mesh(geometry, material);
+sphere.castShadow = true;
+sphere.position.y = 0.5;
+scene.add(sphere);
+
+/**
+ * Light
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
+scene.add(ambientLight);
+
+const directionLight = new THREE.DirectionalLight(0xffffff, 0.2);
+directionLight.castShadow = true;
+directionLight.shadow.mapSize.set(1024, 1024);
+directionLight.shadow.camera.far = 15;
+directionLight.shadow.camera.left = -7;
+directionLight.shadow.camera.top = 7;
+directionLight.shadow.camera.right = 7;
+directionLight.shadow.camera.bottom = -7;
+directionLight.position.set(5, 5, 5);
+scene.add(directionLight);
 
 /**
  * Sizes
@@ -66,7 +145,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.z = 3;
+camera.position.set(-3, 3, 3);
 scene.add(camera);
 
 // Controls
@@ -79,6 +158,8 @@ controls.enableDamping = true;
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
@@ -86,12 +167,17 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  * Animate
  */
 const clock = new THREE.Clock();
-
+let oldElapsedTime = 0;
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-
+  const deltaTime = elapsedTime - oldElapsedTime;
+  oldElapsedTime = elapsedTime;
   // Update controls
   controls.update();
+
+  // Update Physics world
+  world.step(1 / 60, deltaTime, 3);
+  sphere.position.copy(sphereBody.position);
 
   // Render
   renderer.render(scene, camera);
