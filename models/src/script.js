24dd191dd7d38019387ecb887e498e036207373e
Chinmay Shringi <31031919+ChinmayShringi@users.noreply.github.com>
2021-06-02 09:31:01 +0530

    add duck

--- models/src/script.js
+++ models/src/script.js
@@ -1,17 +1,13 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import gsap from "gsap";
 import * as dat from "dat.gui";
-
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 /**
  * Base
  */
 const parameters = {
   color: 0xff0000,
-  spin: () => {
-    gsap.to(mesh.rotation, 1, { y: mesh.rotation.y + Math.PI * 2 });
-  },
 };
 
 // Canvas
@@ -21,12 +17,12 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
- * Object
+ * GLTF MODELS
  */
-const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const gltfLoader = new GLTFLoader();
+gltfLoader.load("/models/Duck/glTF/Duck.gltf", (gltf) => {
+  scene.add(gltf.scene.children[0]);
+});
 
 /**
  * Sizes
