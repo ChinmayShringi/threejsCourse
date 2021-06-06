a47b7aa5c7de9c9a455f7e273c40e0635dfcb0b8
Chinmay Shringi <31031919+ChinmayShringi@users.noreply.github.com>
2021-06-06 09:01:56 +0530

    add water

--- shaders/src/script.js
+++ shaders/src/script.js
@@ -8,6 +8,7 @@ import waterFragmentShader from "./shaders/water/fragment.glsl";
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
+const debugObject = {};
 
 // Scene
 const scene = new THREE.Scene();
@@ -21,22 +22,44 @@ const flagTexture = textureLoader.load("/textures/flag-french.jpg");
  * WATER
  */
 const waterGoemetry = new THREE.PlaneBufferGeometry(2, 2, 128, 128);
+// color
+debugObject.depthColor = "#186691";
+debugObject.surfaceColor = "#9bd8ff";
 
 const waterMaterial = new THREE.ShaderMaterial({
   vertexShader: waterVertexShader,
   fragmentShader: waterFragmentShader,
   side: THREE.DoubleSide,
   uniforms: {
+    // Time
     uTime: {
       value: 0,
     },
+    // BigWaves
     uBigWavesElevation: {
       value: 0.2,
+    },
+    uBigWavesSpeed: {
+      value: 0.75,
     },
     uBigWavesFrequency: {
       value: new THREE.Vector2(4, 1.5),
     },
+    // Color
+    uDepthColor: {
+      value: new THREE.Color(debugObject.depthColor),
+    },
+    uSurfaceColor: {
+      value: new THREE.Color(debugObject.surfaceColor),
+    },
+    uColorOffset: {
+      value: 0.08,
+    },
+    uColorMultiplier: {
+      value: 5,
+    },
   },
+
   // wireframe: true,
   // transparent: true,
 });
@@ -116,6 +139,37 @@ gui
   .max(10)
   .step(0.001)
   .name("uBigWavesElevationY");
+gui
+  .add(waterMaterial.uniforms.uBigWavesSpeed, "value")
+  .min(0)
+  .max(4)
+  .step(0.001)
+  .name("uBigWavesSpeed");
+gui
+  .add(waterMaterial.uniforms.uColorOffset, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uColorOffset");
+gui
+  .add(waterMaterial.uniforms.uColorMultiplier, "value")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("uColorMultiplier");
+
+gui
+  .addColor(debugObject, "depthColor")
+  .name("depthColor")
+  .onChange(() => {
+    waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor);
+  });
+gui
+  .addColor(debugObject, "surfaceColor")
+  .name("surfaceColor")
+  .onChange(() => {
+    waterMaterial.uniforms.uDepthColor.value.set(debugObject.surfaceColor);
+  });
 
 /**
  * Animate
