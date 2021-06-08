18f4f5f137126225cb4c6ece013f46896dde2cab
Chinmay Shringi <31031919+ChinmayShringi@users.noreply.github.com>
2021-06-08 09:55:46 +0530

    Update script.js

--- realisticModels/src/script.js
+++ realisticModels/src/script.js
@@ -90,7 +90,7 @@ material.onBeforeCompile = (shader) => {
     "#include <beginnormal_vertex>",
     `
     #include <beginnormal_vertex>
-    float angle=(position.y+uTime)*0.9;
+    float angle=sin(position.y+uTime)*0.9;
     mat2 rotateMatrix=get2dRotateMatrix(angle);
     objectNormal.xz=rotateMatrix*objectNormal.xz;
 
@@ -124,7 +124,7 @@ depthMaterial.onBeforeCompile = (shader) => {
     `
       #include <begin_vertex>
 
-      float angle=(position.y+uTime)*0.9;
+      float angle=sin(position.y+uTime)*0.9;
       mat2 rotateMatrix=get2dRotateMatrix(angle);
       transformed.xz=rotateMatrix*transformed.xz;
     
