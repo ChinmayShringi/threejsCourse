38f40e66c42aad4d984213a02d2e61dca26da5a3
Chinmay Shringi <31031919+ChinmayShringi@users.noreply.github.com>
2021-06-16 07:56:20 +0530

    compression utils

--- performance/src/script.js
+++ performance/src/script.js
@@ -195,34 +195,63 @@ tick();
 // );
 
 // tip 18 dont create seperate geometry for each diffrent cubes
-const geometry = new THREE.BoxBufferGeometry(0.5, 0.5, 0.5);
+// const geometry = new THREE.BoxBufferGeometry(0.5, 0.5, 0.5);
 
-for (let i = 0; i < 50; i++) {
-  const material = new THREE.MeshNormalMaterial();
+// for (let i = 0; i < 50; i++) {
+//   const material = new THREE.MeshNormalMaterial();
 
-  const mesh = new THREE.Mesh(geometry, material);
-  mesh.position.x = (Math.random() - 0.5) * 10;
-  mesh.position.y = (Math.random() - 0.5) * 10;
-  mesh.position.z = (Math.random() - 0.5) * 10;
-  mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2;
-  mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2;
-  scene.add(mesh);
-}
+//   const mesh = new THREE.Mesh(geometry, material);
+//   mesh.position.x = (Math.random() - 0.5) * 10;
+//   mesh.position.y = (Math.random() - 0.5) * 10;
+//   mesh.position.z = (Math.random() - 0.5) * 10;
+//   mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2;
+//   mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2;
+//   scene.add(mesh);
+// }
 // using utils
-const geometries = [];
+// const geometries = [];
+
+// for (let i = 0; i < 50; i++) {
+//   const geometry = new THREE.BoxBufferGeometry(0.5, 0.5, 0.5);
+//   geometry.translate(
+//     (Math.random() - 0.5) * 10,
+//     (Math.random() - 0.5) * 10,
+//     (Math.random() - 0.5) * 10
+//   );
+//   geometry.rotateX((Math.random() - 0.5) * Math.PI * 2);
+//   geometry.rotateY((Math.random() - 0.5) * Math.PI * 2);
+//   geometries.push(geometry);
+// }
+// const mergedGeometry = BufferGeometryUtils.mergeBufferGeometries(geometries);
+// const material = new THREE.MeshNormalMaterial();
+// const mesh = new THREE.Mesh(mergedGeometry, material);
+// scene.add(mesh);
+
+// tip 22 instance mesh
+const geometry = new THREE.BoxBufferGeometry(0.5, 0.5, 0.5);
+const material = new THREE.MeshNormalMaterial();
+const mesh = new THREE.InstancedMesh(geometry, material, 50);
+// memory management
+mesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
+scene.add(mesh);
 
 for (let i = 0; i < 50; i++) {
-  const geometry = new THREE.BoxBufferGeometry(0.5, 0.5, 0.5);
-  geometry.translate(
+  const position = new THREE.Vector3(
     (Math.random() - 0.5) * 10,
     (Math.random() - 0.5) * 10,
     (Math.random() - 0.5) * 10
   );
-  geometry.rotateX((Math.random() - 0.5) * Math.PI * 2);
-  geometry.rotateY((Math.random() - 0.5) * Math.PI * 2);
-  geometries.push(geometry);
+
+  const quaternion = new THREE.Quaternion();
+  quaternion.setFromEuler(
+    new THREE.Euler(
+      (Math.random() - 0.5) * Math.PI * 2,
+      (Math.random() - 0.5) * Math.PI * 2,
+      0
+    )
+  );
+  const matrix = new THREE.Matrix4();
+  matrix.makeRotationFromQuaternion(quaternion);
+  matrix.setPosition(position);
+  mesh.setMatrixAt(i, matrix);
 }
-const mergedGeometry = BufferGeometryUtils.mergeBufferGeometries(geometries);
-const material = new THREE.MeshNormalMaterial();
-const mesh = new THREE.Mesh(mergedGeometry, material);
-scene.add(mesh);
